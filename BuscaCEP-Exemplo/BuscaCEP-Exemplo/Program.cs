e59969c41a37c67bfee8193184b15cf90fa60032
nichollascorrea <35808408+nichollascorrea@users.noreply.github.com>
2018-03-07 16:44:09 -0300

    Exemplo da Versão 1.0 do pacote BuscaCEP
    Exemplo de uso do pacote BuscaCEP disponível no NuGet

---
+++ BuscaCEP-Exemplo/BuscaCEP-Exemplo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscaCEP_Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CEP cep = new CEP("01001000");
+
+            Console.WriteLine("CEP:" + cep.cep);
+            Console.WriteLine("Logradouro:" + cep.logradouro);
+            Console.WriteLine("Complemento:" + cep.complemento);
+            Console.WriteLine("Bairro:" + cep.bairro);
+            Console.WriteLine("Localidade:" + cep.localidade);
+            Console.WriteLine("CEP:" + cep.uf);
+
+            Console.ReadKey();
+
+        }
+    }
+}
